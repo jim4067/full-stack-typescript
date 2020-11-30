f754b7c7c1e22ded466c8bf14ae046b2e21f93ec
jim4067 <jimmyimpulse2@gmail.com>
2020-11-30 20:12:47 +0300

    feat: calculating the exercises by using the array in the req bodys

--- exercises-calc.ts
+++ exercises-calc.ts
@@ -5,28 +5,29 @@ interface Result {
     average_time: number,
     isAchieved: boolean
     rating: number,
-    rating_explained: String
+    rating_explained: string
 }
 
-let exercise_days: Array<number> = [];
-
-//get hours for the days the person worked out
-//the agv starts at three because two is reserved for the target_hours
-for (let index = 3; index <= process.argv.length; index++) {
-    if (!isNaN(Number(process.argv[index]))) {
-        exercise_days.push(Number(process.argv[index]))
-    }
-
-}
+const exercise_days: Array<number> = [];
 
 // calculate_exercises([3, 0, 2, 4.5, 0, 3, 1]);
-const calculate_exercises = (): Result => {
+const calculate_exercises = (param: Array<string | number>): Result => {
+
+    //get hours for the days the person worked out
+    //the array listing starts at two because one  is reserved for the target.
+    for (let index = 1; index <= param.length; index++) {
+        if (!isNaN(Number(param[index]))) {
+            exercise_days.push(Number(param[index]));
+        }
+
+    }
+
     const period_length = exercise_days.length;
 
     //get the days he trained. days with zero
     const training_days = exercise_days.filter((days) => days !== 0).length;
 
-    const target_hours = Number(process.argv[2]);
+    const target_hours = Number(param[1]);
 
     const average_time = exercise_days.reduce((acc, curVal) => acc + curVal) / period_length;
 
@@ -44,10 +45,11 @@ const calculate_exercises = (): Result => {
         isAchieved,
         rating,
         rating_explained
-    }
-}
+    };
+};
 
-console.log(calculate_exercises());
+export { calculate_exercises };
+// console.log(calculate_exercises());
 
 //@jim4067
 // A function  that calculates the average time of daily exercise hours
--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
-const express = require('express');
+// const express = require('express');
+import express from 'express';
 import { calculateBmi } from './bmi-calculator';
+import { calculate_exercises } from './exercises-calc';
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/hello', (req, res) => {
     res.send("Hello full stack");
@@ -17,6 +21,14 @@ app.get('/bmi', (req, res) => {
     const bmi = calculateBmi(Number(weight), Number(height));
 
     res.json({ height, weight, "bmi": bmi });
+});
+
+app.post('/exercicises', (req, res) => {
+    const { exercise_params } = req.body;
+
+    const result = calculate_exercises(exercise_params);
+
+    res.send(result);
 });
 
 const PORT = 3030;
